77a6d4ce8cda91917f70565b697612f7dc803686
leo <847301609@qq.com>
2019-05-18 09:07:42 +0800

    add devServer

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -2,39 +2,45 @@
  * 本地预览
  */
 
-const path = require('path');
-const merge = require('webpack-merge');
-const webpackBaseConfig = require('./webpack.config.base.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require("path");
+const merge = require("webpack-merge");
+const webpackBaseConfig = require("./webpack.config.base.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(webpackBaseConfig, {
-  mode:'development',
+  mode: "development",
   // 入口
   entry: {
-    main: './example/index',
-    vendors: ['vue'],
+    main: "./example/index",
+    vendors: ["vue"]
   },
   // 输出
   output: {
-    path: path.join(__dirname, '../example/dist'),
-    publicPath: '',
-    filename: '[name].js',
-    chunkFilename: '[name].chunk.js',
+    path: path.join(__dirname, "../example/dist"),
+    publicPath: "",
+    filename: "[name].js",
+    chunkFilename: "[name].chunk.js"
   },
   resolve: {},
   optimization: {
     splitChunks: {
-      chunks: 'all',
-    },
+      chunks: "all"
+    }
   },
-  devtool: '#eval-source-map',
+  devtool: "#eval-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      filename: 'index.html',
-      template: path.join(__dirname, '../example/index.html'),
-    }),
-    new BundleAnalyzerPlugin()
+      filename: "index.html",
+      template: path.join(__dirname, "../example/index.html")
+    })
+    // new BundleAnalyzerPlugin()
   ],
+  devServer: {
+    port: 8000,
+    compress: true,
+    open: true,
+    hot: true
+  }
 });
